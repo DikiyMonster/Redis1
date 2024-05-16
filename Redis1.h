8da26e1e985eef0c5fb54a82fd1e74844691a439
DikiyMonster <86494269+DikiyMonster@users.noreply.github.com>
2024-05-16 22:07:03 +0400

    Redis1.h

--- Redis1.h
+++ Redis1.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <mutex>
 #include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -13,21 +14,20 @@ private:
     unordered_map<string, pair<int, chrono::system_clock::time_point>> data;
     mutex mtx;
     bool isEnablePersistence;
-    chrono::system_clock::time_point lastCleanupTime; // Added member for last cleanup time
+    chrono::system_clock::time_point lastCleanupTime;
+    thread cleanupThread;
 
 public:
     MyRedis(bool enablePersistence = true);
+    ~MyRedis();
 
     void set(const string& key, int value, chrono::seconds expiration = chrono::seconds(0));
-
     int get(const string& key);
 
 private:
     void saveToDisk();
-
     void loadFromDisk();
-
-    void cleanup(); 
+    void cleanup();
 };
 
 #endif
